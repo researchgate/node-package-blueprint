8c3c5234291b73c357c60911c5c51c110f99fbf4
Luis Merino <mail@luismerino.name>
2017-08-31 15:41:55 +0200

    chore(eslint-format): eslint config and format overhaul

--- postcss.config.js
+++ postcss.config.js
@@ -2,11 +2,11 @@ const cssnano = require('cssnano');
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
-  map: false,
-  plugins: [
-    autoprefixer(),
-    cssnano({
-      preset: 'default',
-    }),
-  ],
+    map: false,
+    plugins: [
+        autoprefixer(),
+        cssnano({
+            preset: 'default',
+        }),
+    ],
 };
