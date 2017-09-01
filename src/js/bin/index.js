1bf07205da10d1ca016b84ee9ad5b23e870463d7
Roland Lösslein <roland.loesslein@researchgate.net>
2017-09-01 09:57:35 +0200

    feat(validation): Add validation and better cli output

--- src/js/bin/index.js
+++ src/js/bin/index.js
@@ -1,10 +1,13 @@
 import packageJson from '../../../package.json';
+import validateProjectName from 'validate-npm-package-name';
+import semver from 'semver';
 import commander from 'commander';
 import readline from 'readline';
 import chalk from 'chalk';
 import path from 'path';
 import fs from 'fs';
 import mkdirp from 'mkdirp';
+import { Spinner } from 'cli-spinner';
 import { ncp } from 'ncp';
 
 let directoryValue;
@@ -57,16 +60,22 @@ const promptForPackageJson = (key, question) =>
         const defaultConfig = packageJsonNew[key] ? chalk.grey(` ${packageJsonNew[key]}`) : '';
         prompt(`${question}${defaultConfig} `).then(answer => {
             packageJsonNew[key] = answer || packageJsonNew[key];
-            resolve(answer);
+            resolve(packageJsonNew[key]);
         });
     });
 
 // prompt('What is your name?').then((name) => {console.log(`Oh, hi ${name}`)});
 promptForPackageJson('name', 'package name:')
-    .then(() => {
+    .then(name => {
+        if (!isNameValid(name)) {
+            process.exit(1);
+        }
         return promptForPackageJson('version', 'version:');
     })
-    .then(() => {
+    .then(version => {
+        if (!isVersionValid(version)) {
+            process.exit(1);
+        }
         return promptForPackageJson('description', 'description:');
     })
     .then(() => {
@@ -80,17 +89,90 @@ promptForPackageJson('name', 'package name:')
         createProject(directoryValue, config);
     });
 
-const isSafeToCreateProject = dir => {};
+const printValidationResults = results => {
+    if (typeof results !== 'undefined') {
+        results.forEach(error => {
+            console.error(chalk.red(`  *  ${error}`));
+        });
+    }
+};
+
+const isVersionValid = version => {
+    const validationResult = semver.valid(version);
+
+    if (!validationResult) {
+        console.error(
+            `Could not create a project with version ${chalk.red(
+                `"${version}"`,
+            )} because of invalid semantic version format.`,
+        );
+        return false;
+    }
+    return true;
+};
+
+const isNameValid = name => {
+    const validationResult = validateProjectName(name);
+    if (!validationResult.validForNewPackages) {
+        console.error(
+            `Could not create a project called ${chalk.red(`"${name}"`)} because of npm naming restrictions:`,
+        );
+        printValidationResults(validationResult.errors);
+        printValidationResults(validationResult.warnings);
+        return false;
+    }
+    return true;
+};
+
+const isSafeToCreateProjectIn = (dir, name) => {
+    const validFiles = [
+        '.DS_Store',
+        'Thumbs.db',
+        '.git',
+        '.gitignore',
+        '.idea',
+        'README.md',
+        'LICENSE',
+        'web.iml',
+        '.hg',
+        '.hgignore',
+        '.hgcheck',
+    ];
+    console.log();
+
+    const conflicts = fs.readdirSync(dir).filter(file => !validFiles.includes(file));
+    if (conflicts.length < 1) {
+        return true;
+    }
+
+    console.log(`The directory ${chalk.green(name)} contains files that could conflict:`);
+    console.log();
+    for (const file of conflicts) {
+        console.log(`  ${file}`);
+    }
+    console.log();
+    console.log('Either try using a new directory name, or remove the files listed above.');
+
+    return false;
+};
 
 const createProject = (name, config) => {
-    console.log();
-    console.log("👍🏻  Roger that. Please lean back while we'll boostrap your project. That may take a while.");
-    console.log();
-
     const root = path.resolve(name);
     const appName = path.basename(root);
 
     mkdirp.sync(name);
+
+    if (!isSafeToCreateProjectIn(root, name)) {
+        process.exit(1);
+    }
+
+    console.log();
+    console.log('👍🏻  Roger that.');
+    console.log();
+
+    const spinner = new Spinner('%s Please wait while we`re bootstrapping your project');
+    spinner.setSpinnerString('⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏');
+    spinner.start();
 
     ncp(path.resolve(__dirname, '../../../'), root, err => {
         if (err) {
@@ -106,9 +188,11 @@ const createProject = (name, config) => {
         fs.writeFileSync(path.join(root, 'package.json'), JSON.stringify(newPackageJson, null, 2));
 
         ncp(path.resolve(__dirname, '../../../template'), root, err => {
+            console.log();
+            console.log();
             console.log(`🙌🏻  Your project is ready and is waiting for you over here: ${root}`);
             console.log();
-            console.log(`Happy coding.`);
+            spinner.stop();
         });
     });
 };
